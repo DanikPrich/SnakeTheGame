2403b62993172a639aaba6835e21184fab7843ae
Daniel <Danil.danek@gmail.com>
2023-04-26 18:13:32 +0200

    mobile adaptation, redesign

--- src/components/gameField.vue
+++ src/components/gameField.vue
@@ -1,23 +1,21 @@
 <template>
 <div class="game__field">
 	<!-- eslint-disable-next-line -->
-	<div v-for="line in $field" class="game__line">
+	<div v-for="(line, indexLine) in $field" class="game__line">
 		<!-- eslint-disable-next-line -->
 		<div 
-		v-for="point in line" 
-		class="game__block"
-		
+			v-for="(value, indexRow) in line" 
+			class="game__block"
+			:style="{ backgroundColor: (indexLine + indexRow) % 2 == 0 ? '#f0f8ff' : '#e6f4ff'}"
 		> 
+			<!-- {{ (indexLine + indexRow) % 2 == 0 ? 1 : 0 }} -->
 			<div :class="{
-				'game__block-head' : point == 0,
-				'game__block-body' : point == 1,
-				'game__block-food': point == 2
+				'game__block-head' : value == 0,
+				'game__block-body' : value == 1,
+				'game__block-food': value == 2
 			}">
 
 			</div>
-			<!-- <div class="game__block-food">
-
-			</div> -->
 		</div>
 	</div>
 </div>
@@ -43,36 +41,51 @@ import { mapState } from 'vuex'
 <style lang="sass" scoped>
 
 .game
-	&__field
 	&__line
 		display: flex
 	&__block
-		width: 23px
-		height: 23px
+		width: 16px
+		height: 16px
 		margin: 0
-		border: 0.5px white solid
-		background-color: lightgrey
+		// border: 0.5px lightgray solid
+		background-color: #ffff
 		&-food
 			// background:  url('../img/apple.png')
 			width: 100%
 			height: 100%
 			background: center / contain no-repeat url("../img/apple.png")
-			background-color: lightgrey
+			// background-color: lightgrey
 		&-head
 			width: 100%
 			height: 100%
-			background-color: #ff7c00
+			background-color: #179c1c
 			border-radius: 100%
 			color: grey
 		&-body
 			width: 100%
 			height: 100%
-			background-color: #40ae1a
+			background-color: #23d929
 			border-radius: 100%
 
 		
 		
 
+@media (min-width: 330px)
+	.game
+		&__block
+			width: 20px
+			height: 20px
 
+@media (min-width: 500px)
+	.game
+		&__block
+			width: 30px
+			height: 30px
+
+@media (min-width: 800px)
+	.game
+		&__block
+			width: 50px
+			height: 50px
 
 </style>
